e27c6acb484245bac96cb6a24527966f317e2e18
pragneshnp14 <pragneshnp1430@gmail.com>
2024-06-29 09:07:37 +0000

    datatype summary

---
+++ datatype_summary.js
@@ -0,0 +1,18 @@
+// primitive
+
+
+// 7 types: String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 665565656566565n;
+
+// console.log(id);
+console.log(typeof bigNumber);
+
+console.log(id === anotherId);
+
+// reference (non primitive)
+
+// Array, Objects, Functions
