e8a7805c450328e46da7ac9518f5d99cbe9c3fbd
pragneshnp14 <pragneshnp1430@gmail.com>
2024-06-29 08:03:02 +0000

    conversion datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// undefine => NaN
+// null => 0
+
+
+let isLoggedIn = "null"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;
+// "" => false
+// "prem" => true
+
+
+let someNumber = 5566
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
