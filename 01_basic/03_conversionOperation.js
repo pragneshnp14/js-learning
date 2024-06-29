d5aecf1a76b472ab8f807b6763fd3f235b5a91e3
pragneshnp14 <pragneshnp1430@gmail.com>
2024-06-29 08:51:52 +0000

    comparison

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,10 +23,58 @@ let isLoggedIn = "null"
 // 1 => true;
 // "" => false
 // "prem" => true
-
-
 let someNumber = 5566
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations *********************
+ 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(typeof negValue);
+
+// console.log(2+2); //add
+// console.log(2-2); //sub
+// console.log(2*2); //mul
+// console.log(2/2); //divide
+// console.log(2**2); //use for power of 2
+// console.log(2%2); //reminder check
+
+let str1 = "hello"
+let str2 = "pragnesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//console.log("1" + 2); //12
+//console.log(1 + "2"); //12
+// console.log("1" + "2");  //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true++);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2 // no do like this
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
