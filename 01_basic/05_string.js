3e7959435a1656a9025407001c64fd00e1d9ca32
pragneshnp14 <pragneshnp1430@gmail.com>
2024-06-29 10:29:17 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,33 @@
+const name ="pragnesh"
+const repoCount = 46
+
+// console.log(name + " " + repoCount + " value");
+// console.log(` Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('pragnesh-pc');
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('g'));
+
+
+const newString = gameName.substring(8,11)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8,8)
+// console.log(anotherString);
+
+const newString1 = "    pagnesh    "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const newString2 = "pragnesh n patel"
+console.log(newString2);
+console.log(newString2.replace('patel','n patel'))
+console.log(newString2.includes('n'))
+
+console.log(newString2.split(' '))
+
+
+
